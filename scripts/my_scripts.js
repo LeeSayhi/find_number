a11ad7638b0e478d23c94238f8ce60a19239965e
zhaoli <leesayhi@126.com>
2017-02-08 09:48:58 +0800

    first commit

--- scripts/my_scripts.js
+++ scripts/my_scripts.js
@@ -1,51 +1,55 @@
 $(document).ready(function() {
-
-	$(".guess_box").click( checkForCode );            //单击guess_box类时调用checkForCode函数
-	
-	function getRandom(num){                            //随机数生成函数
-		var my_num = Math.floor(Math.random()*num);    //声明变量my_num,并赋值0-num之间的随机整数
-		return my_num;                                //返回随机数
+	$(".guess_box").click( checkForCode );            
+		function getRandom(num){                            
+		var my_num = Math.floor(Math.random()*num);    
+		return my_num;                                
 	}
 	
-	var hideCode = function(){              //命名函数（折扣每次会隐藏在不同的方框里）
-	  var numRand = getRandom(4);          //调用随机函数getRandom         
-	  $(".guess_box").each(function(index, value) {                         //遍历各个guess_box类
-		 if(numRand == index){            //将当前的列表位置与随机数比较
-			$(this).append("<span id='has_discount'></span>");             //将ID为has_discount的span元素增加到.guess_box类中      
-			return false;               //跳出.each()循环
+	//  生产随机数随机隐藏到某个方框中
+	var hideCode = function(){              
+	  var numRand = getRandom(4);              
+	  $(".guess_box").each(function(index, value) {                         
+		 if(numRand == index){            
+			$(this).append("<span id='has_discount'></span>");                   
+			return false;              
 		 } 
 	  });
 	}
+
+	// 调用隐藏数字变量的命名函数
+	hideCode();    
 	
-	hideCode();    //调用隐藏折扣变量的命名函数hideCode
-	
-	function checkForCode(){    //用来显示折扣码的函数
-		var discount;     //声明discount变量
-	 	if($.contains(this, document.getElementById("has_discount") ) )     //条件逻辑，查看用户是否找到折扣码（查找
-		{                                                                  // 一个ID为has_discount的DOM元素是否在当前元素里)
-			var my_num = getRandom(100);    //调用随机函数getRandom
-			discount = "<p>Your Code: CODE"+my_num +"</p>";                 // 设置消息，
-		}else{                                                              // 根据用户是否找到折扣码
-			discount = "<hr>Sorry, no discount this time!" ;                // 消息会有所不同
+	// 用来显示数字，并根据用户的选择做出不同的反应
+	function checkForCode(){    
+		var discount;     
+		// 查看用户是否找到数字
+	 	if($.contains(this, document.getElementById("has_discount") ) )     
+		{                                                                
+			var my_num = getRandom(100);    
+			discount = "<hr>恭喜您，接下来"+my_num +"天都会持续好运哦，抓紧时间去表白吧!";                 
+		}else{                                                              
+			discount = "<hr>抱歉，继续加油找! （正确的是绿色边框里面的!）" ;                
 		}
 
-		$(".guess_box").each(function() {               //遍历各个guess_box类
-			if($.contains(this, document.getElementById("has_discount") ) )     //查看这个方框是否包含折扣码
+		$(".guess_box").each(function() {        
+			// 查看这个方框是否包含数字      
+			if($.contains(this, document.getElementById("has_discount") ) )    
 			{
-				$(this).addClass("discount");                                  //如果找到，改变方框外观，并告诉用户折扣码是...
+				$(this).addClass("discount");                                  
 			}else{
-				$(this).addClass("no_discount");                               //如果没找到，直接提示给用户
+				$(this).addClass("no_discount");                               
 			}
-			$(this).unbind();                                                 //解除监听事件
+			$(this).unbind();                                                
 		});
-		$("#result").append(discount);                                      //把输出的消息写到页面上单独区域中
-	}                                                                       // End checkForCode function 
+		$("#result").append(discount);                                     
+	}                                                                       
 	
-	$(".guess_box").hover(                                                 //增加hover事件
+	// 增加hover事件
+	$(".guess_box").hover(                                                 
 	  function () {
-	 	$(this).addClass("my_hover");                                     //当用户停在这个方框上时添加my_hover CSS类
+	 	$(this).addClass("my_hover");                                     
 	   },
 	   function () {
-		  $(this).removeClass("my_hover");                              //移开时删除my_hover CSS类
-	   });  // End hover
+		  $(this).removeClass("my_hover");                             
+	   });  
 });
